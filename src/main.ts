2edbf746d3a7b4efd81093c38ac2f2c0c236e430
doyoung0205 <doyoung0205@naver.com>
2020-11-26 16:14:05 +0900

    remove 구글 애널리틱스

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import VueAnalytics from "vue-analytics";
 import VueGtag from "vue-gtag";
 
 import App from "./App.vue";
@@ -9,9 +8,6 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueAnalytics, {
-  id: "UA-183952985-1" // Google Analytics의 Tracking ID를 넣어준다
-});
 Vue.use(VueGtag, {
   config: {
     id: "GTM-WJ8G2WK" // Google Analytics의 Tracking ID를 넣어준다
