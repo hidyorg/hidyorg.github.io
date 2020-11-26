7c4b0f196adc55b49be82a38f84559d69e5c5d1b
doyoung0205 <doyoung0205@naver.com>
2020-11-26 20:19:18 +0900

    remove hotjar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueGtag from "vue-gtag";
-import Hotjar from "vue-hotjar";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -12,11 +11,6 @@ Vue.use(VueGtag, {
   config: {
     id: "GTM-WJ8G2WK" // Google Analytics의 Tracking ID를 넣어준다
   }
-});
-
-Vue.use(Hotjar, {
-  id: "2122391",
-  isProduction: process.env.NODE_ENV === "production"
 });
 
 new Vue({
