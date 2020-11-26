71ab07ebeb43936c06684d4335c113384838bca5
doyoung0205 <doyoung0205@naver.com>
2020-11-26 14:12:19 +0900

    add 분석 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import Vue from "vue";
+import VueAnalytics from "vue-analytics";
+import VueGtag from "vue-gtag";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "G-SZRG4R6P3L" // Google Analytics의 Tracking ID를 넣어준다
+});
+Vue.use(VueGtag, {
+  config: {
+    id: "G-SZRG4R6P3L" // Google Analytics의 Tracking ID를 넣어준다
+  }
+});
 
 new Vue({
   router,
